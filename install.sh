a26ea894b36ee9ad8050666e19da5f5c8dd4610a
poiega <110189813+poiega@users.noreply.github.com>
2022-11-08 02:52:45 +0900

    완료.

--- install.sh
+++ install.sh
@@ -7,4 +7,9 @@ wget https://theseed.io/js/intersection-observer.js -P /js/
 wget https://theseed.io/js/dateformatter.js -P /js/
 wget https://theseed.io/css/wiki.css -P /css/
 wget https://theseed.io/css/katex.min.css -P /css/
-wget https://theseed.io/css/diffview.csss -P /css/
+wget https://theseed.io/css/diffview.css -P /css/
+cd skins
+wget https://github.com/dorincop/buma/archive/refs/heads/main.zip
+unzip main.zip
+wget https://github.com/dorincop/liberty/archive/refs/heads/main.zip
+unzip main (1).zip
