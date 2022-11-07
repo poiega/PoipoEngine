684eb9ea6a225be6ea4238d10fce0d55601a3c1e
poiega <110189813+poiega@users.noreply.github.com>
2022-11-08 02:46:40 +0900

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "PoipoEngine 엔진 구동 필요 파일 자동 다운기"
+echo "개발자 : poiega"
+wget https://theseed.io/js/theseed.js -P /js/
+wget https://theseed.io/js/jquery-2.1.4.min.js -P /js/
+wget https://theseed.io/js/jquery-1.11.3.min.js -P /js/
+wget https://theseed.io/js/intersection-observer.js -P /js/
+wget https://theseed.io/js/dateformatter.js -P /js/
+wget https://theseed.io/css/wiki.css -P /css/
+wget https://theseed.io/css/katex.min.css -P /css/
+wget https://theseed.io/css/diffview.csss -P /css/
